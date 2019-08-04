591a8042b8f06c602bfa1ea30cbd80e0cf6d90ba
Nicholas <dojoman19@gmail.com>
2019-08-03 23:37:16 -0400

    Added initial route

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/labstack/echo"
+import (
+	"github.com/labstack/echo"
+	"github.com/zedjones/redirectprotect/routes"
+)
 
 func main() {
 	e := echo.New()
-	e.Renderer = nil
+	e.POST("/add_redirect", routes.RegisterURL)
+	e.Start(":1234")
 }
