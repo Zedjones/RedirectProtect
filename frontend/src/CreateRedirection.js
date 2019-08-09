4f6c4eb2513db2ffd5062a79a0a6642e7f6c0b58
Nicholas <dojoman19@gmail.com>
2019-08-08 22:35:27 -0400

    Got basic time picker working

--- frontend/src/CreateRedirection.js
+++ frontend/src/CreateRedirection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -6,8 +6,11 @@ import TextField from '@material-ui/core/TextField';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { KeyboardTimePicker, TimePicker } from "@material-ui/pickers";
 import Container from '@material-ui/core/Container';
 import lifecycle from 'react-pure-lifecycle';
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import LuxonUtils from "@date-io/luxon"
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -42,53 +45,69 @@ const methods = {
 
 function SignIn() {
   const classes = useStyles();
+  const [selectedDate, handleDateChange] = useState(new Date());
+  handleDateChange(selectedDate.setHours(0, 10, 0, 0))
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Shorten URL
+    <MuiPickersUtilsProvider utils={LuxonUtils}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Shorten & Encrypt URL
         </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
-            type="url"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="URL"
+              name="username"
+              autoComplete="username"
+              type="url"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Passphrase"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <KeyboardTimePicker
+              clearable
+              ampm={false}
+              autoOk={true}
+              views={["hours", "minutes"]}
+              label="Hours and minutes"
+              openTo="minutes"
+              format="HH:mm"
+              placeholder="00:10"
+              value={selectedDate}
+              onChange={(val) => {handleDateChange(val); console.log(selectedDate)}}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
           </Button>
-        </form>
-      </div>
-    </Container>
+          </form>
+        </div>
+      </Container>
+    </MuiPickersUtilsProvider>
   );
 }
 
