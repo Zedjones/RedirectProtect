b3c1356e13e654363fd829ced9ea73873e61719f
Nicholas <dojoman19@gmail.com>
2019-08-10 19:57:52 -0400

    Added checkbox to toggle duration selection

--- frontend/src/CreateRedirection.js
+++ frontend/src/CreateRedirection.js
@@ -5,6 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
+import Checkbox from '@material-ui/core/Checkbox';
 import { makeStyles } from '@material-ui/core/styles';
 import { KeyboardTimePicker } from "@material-ui/pickers";
 import Container from '@material-ui/core/Container';
@@ -13,6 +14,7 @@ import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import LuxonUtils from "@date-io/luxon"
 import { DateTime } from "luxon"
 import AccessTimeIcon from "@material-ui/icons/AccessTime"
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -50,67 +52,81 @@ function SignIn() {
   const [selectedDate, handleDateChange] = useState(new DateTime.fromObject({ hours: 0, minutes: 0 }));
 
   return (
-    <MuiPickersUtilsProvider utils={LuxonUtils}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Shorten & Encrypt URL
-        </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="URL"
-              name="username"
-              autoComplete="username"
-              type="url"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Passphrase"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <KeyboardTimePicker
-              clearable
-              ampm={false}
-              autoOk={true}
-              views={["hours", "minutes"]}
-              label="Shortened URL Lifespan"
-              openTo="minutes"
-              format="HH:mm"
-              keyboardIcon={React.createElement(AccessTimeIcon, null)}
-              placeholder="00:10"
-              value={selectedDate}
-              style={{width: "100%"}}
-              onChange={(val) => { handleDateChange(val); console.log(selectedDate) }}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Encrypt
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Grid container display='flex' direction='column' alignItems='center'>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Shorten & Encrypt URL
+          </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="URL"
+            name="username"
+            autoComplete="username"
+            type="url"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Passphrase"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Grid container direction='row' alignItems='center'>
+            <Grid item xs={2}>
+              <Checkbox
+                value="checkedA"
+                inputProps={{
+                  'aria-label': 'primary checkbox',
+                }}
+              />
+              </Grid>
+              <Grid item xs={10}>
+              <MuiPickersUtilsProvider utils={LuxonUtils}>
+                <KeyboardTimePicker
+                  clearable
+                  ampm={false}
+                  margin="normal"
+                  autoOk={true}
+                  views={["hours", "minutes"]}
+                  inputVariant="outlined"
+                  label="Shortened URL Lifespan"
+                  openTo="minutes"
+                  format="HH:mm"
+                  keyboardIcon={React.createElement(AccessTimeIcon, null)}
+                  placeholder="hh:mm"
+                  value={selectedDate}
+                  style={{ width: "100%" }}
+                  onChange={(val) => { handleDateChange(val); console.log(selectedDate) }}
+                />
+              </MuiPickersUtilsProvider>
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Encrypt
           </Button>
-          </form>
-        </div>
-      </Container>
-    </MuiPickersUtilsProvider>
+        </form>
+      </Grid>
+    </Container >
   );
 }
 
