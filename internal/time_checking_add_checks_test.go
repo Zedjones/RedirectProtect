2651a0506863f75c46297c1bd4f0355c1804d63e
Nicholas <dojoman19@gmail.com>
2019-08-22 11:03:10 -0400

    Finished tests for AddChecks

--- internal/time_checking_add_checks_test.go
+++ internal/time_checking_add_checks_test.go
@@ -20,6 +20,9 @@ func TestAddChecks(t *testing.T) {
 		g.It("should fail when it cannot deepcopy a redirection", func() {
 			g.Assert(testDeepcopyFail(t, g)).Equal(errors.New("some error"))
 		})
+		g.It("should succeed when nothing above is happening", func() {
+			g.Assert(testSuccess(t, g)).Equal(nil)
+		})
 	})
 }
 
@@ -60,6 +63,44 @@ func testDeepcopyFail(t *testing.T, g *goblin.G) error {
 		oldRedir := old.(**db.Redirect)
 		g.Assert((*oldRedir).Path).Equal("test")
 		return errors.New("some error")
+	}
+
+	return AddChecks()
+}
+
+func testSuccess(t *testing.T, g *goblin.G) error {
+	ctrl := gomock.NewController(t)
+	mockConnection := mocks.NewMockConnection(ctrl)
+	mockCollection := mocks.NewMockCollection(ctrl)
+	mockResultSet := mocks.NewMockResultSet(ctrl)
+
+	mockResultSet.EXPECT().Next(&db.Redirect{}).DoAndReturn(
+		func(redir *db.Redirect) bool {
+			redir.Path = "test"
+			return true
+		})
+	mockResultSet.EXPECT().Next(&db.Redirect{Path: "test"}).DoAndReturn(
+		func(redir *db.Redirect) bool {
+			redir.Path = "test"
+			return false
+		})
+	mockCollection.EXPECT().Find(bson.M{}).Return(mockResultSet)
+	mockConnection.EXPECT().Collection("redirections").Return(mockCollection)
+
+	oldGetConn := getConnection
+	defer func() { getConnection = oldGetConn }()
+
+	getConnection = func() (db.Connection, error) {
+		return mockConnection, nil
+	}
+
+	oldStartTimeCheck := startTimeCheck
+	defer func() { startTimeCheck = oldStartTimeCheck }()
+
+	startTimeCheck = func(redir *db.Redirect, collection db.Collection) error {
+		g.Assert(redir.Path).Equal("test")
+		g.Assert(collection).Equal(mockCollection)
+		return nil
 	}
 
 	return AddChecks()
