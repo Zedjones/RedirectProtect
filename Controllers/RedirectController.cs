109b4532b5bec41f6505ed8829fb1efb6b383006
Zedjones <dojoman19@gmail.com>
2020-01-17 20:58:03 -0500

    Added basic controller to add/get redirects

---
+++ Controllers/RedirectController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedirectProtect.Services;
+using RedirectProtect.Database.Models;
+
+namespace RedirectProtect.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RedirectController : ControllerBase
+    {
+        private readonly RedirectService _redirectService;
+
+        public RedirectController(RedirectService redirectService)
+        {
+            _redirectService = redirectService;
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetRedirect")]
+        public ActionResult<Redirect> GetAction(string id)
+        {
+            var redirect = _redirectService.GetRedirect(id);
+
+            if (redirect is null)
+            {
+                return NotFound();
+            }
+
+            return redirect;
+        }
+
+        [HttpPost]
+        public ActionResult<Redirect> Create(Redirect redirect)
+        {
+            _redirectService.Create(redirect);
+
+            return CreatedAtRoute("GetBook", new { id = redirect.Id });
+        }
+    }
+}
