45d9cf7c9472fb063e527f7f267d59956a1e6ea8
Zedjones <dojoman19@gmail.com>
2020-02-05 09:40:39 -0500

    Continued working on CreateToast

--- ClientApp/src/CreateToast.jsx
+++ ClientApp/src/CreateToast.jsx
@@ -2,6 +2,8 @@ import { SnackbarProvider } from "notistack";
 import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
+import CloseIcon from '@material-ui/icons/Close';
+import { IconButton } from "@material-ui/core";
 
 function Alert(props) {
     return <MuiAlert elevation={0} variant="filled" {...props} />
@@ -10,17 +12,30 @@ function Alert(props) {
 export default function CreateToast(props) {
     const [open, setOpen] = [props.open, props.setOpen];
     const path = props.path
+    const notistackRef = React.createRef();
+    const onClickDismiss = key => () => {
+        notistackRef.current.closeSnackbar(key);
+    }
 
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
-          return;
+            return;
         }
-    
+
         setOpen(false);
-      };
+    };
 
     return (
-        <SnackbarProvider>
+        <SnackbarProvider
+            ref={notistackRef}
+            action={(key) => (
+                <React.Fragment>
+                    <IconButton size="small" aria-label="close" color="inherit" onClick={onClickDismiss(key)}>
+                        <CloseIcon fontSize="small" />
+                    </IconButton>
+                </React.Fragment>
+            )}
+            anchorOrigin={{ horizontal: "center" }}>
             <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
                 <Alert onClose={handleClose} severity="success">
                     Created shortened link at {path}
