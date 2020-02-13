f921942cc3e4a1ffca0ec2e69a2e20ed03e59ee0
Zedjones <dojoman19@gmail.com>
2020-02-13 10:45:18 -0500

    Forgot to save this

--- Tests/ControllerTests/MainControllerTest.cs
+++ Tests/ControllerTests/MainControllerTest.cs
@@ -5,6 +5,11 @@ namespace RedirectProtect.Tests.ControllerTests
     public class MainControllerTest
     {
         [Fact]
+        public void GetRedirect_Invalid_Path()
+        {
+            
+        }
+        [Fact]
         public void PassingTest()
         {
             Assert.Equal(4, Add(2, 2));
